343caf63c7d8c2ce0c1d047fae2c178cd1015e9f
Preetgill786 <57011583+Preetgill786@users.noreply.github.com>
2020-01-16 14:23:27 -0500

    Add files via upload

--- My Map/CurrentLocationViewController.swift
+++ My Map/CurrentLocationViewController.swift
@@ -13,21 +13,46 @@ import CoreData
 class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
     
     //var managedContext: NSManagedObjectContext!
+     var refreshControl = UIRefreshControl()
+      var location = Map()
     
+    var mTitle = ""
+       var mSubTitle = ""
+       var mLatitude = 0.0
+       var mLongitude = 0.0
+       var locArry = [Map]()
+       var pinsArry = [Map]()
  
-    @IBOutlet weak var mTitle: UITextField!
+    @IBOutlet weak var tTitle: UITextField!
     @IBOutlet weak var subTitle: UITextField!
   
     @IBOutlet weak var longititude: UITextField!
     @IBOutlet weak var latitude: UITextField!
     @IBOutlet weak var doneBtn: UIButton!
     
+    @IBOutlet weak var saveBtn: UIBarButtonItem!
     @IBAction func save(_ sender: UIBarButtonItem) {
         self.navigationController?.popToRootViewController(animated: true)
     }
     
     @IBAction func donebtn(_ sender: UIButton) {
-        
+        self.saveBtn.isEnabled = true
+
+               let tt = tTitle.text
+               let st = subTitle.text
+               let lt = latitude.text
+               let lgt = longititude.text
+               
+                self.fetchAndUpdateTable()
+               mTitle = tt!
+               mSubTitle =  st!
+               mLatitude =  Double(lt!)!
+               mLongitude =  Double(lgt!)!
+               
+              self.insertRecord(title: mTitle, subTitle: mSubTitle, latitude: mLatitude, longitude: mLongitude)
+                self.fetchAndUpdateTable()
+               self.view.setNeedsLayout()
+              // self.tableView.reloadData()
         
     }
     
@@ -38,11 +63,80 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-    }
-    
-    
+        
+        pinsArry = locArry
+               self.saveBtn.isEnabled = false
+  
     
 }
+
+    override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
+
+          // managedContext = coreDataStack.managedContext
+       refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+   refreshControl.addTarget(self, action: #selector(refresh), for: UIControl.Event.valueChanged)
+//           tableView.addSubview(refreshControl)
+         fetchAndUpdateTable()
+
+             
+       }
+
+     @objc func refresh() {
+           
+           
+            self.refreshControl.endRefreshing()
+        }
+        func fetchAndUpdateTable(){
+               locArry = fetchRecords()
+               pinsArry = locArry
+
+              
+           }
+    
+    func insertRecord(title:String, subTitle:String, latitude: Double, longitude: Double){
+          let location = Map(context: ViewController.managedContext)
+             
+                   location.title = title
+                   location.subTitle = subTitle
+                   location.latitude = latitude
+                   location.longitude = longitude
+                   
+          try! ViewController.managedContext.save()
+               }
+         
+      
+      
+         func fetchRecords() -> [Map]{
+            var arrLocation = [Map]()
+            let fetchRequest = NSFetchRequest<Map>(entityName: "Map")
+            
+                do{
+                  arrLocation = try ViewController.managedContext.fetch(fetchRequest)
+                }catch{
+                    print(error)
+                }
+                return arrLocation
+            }
+
+      
+            func deleteRecord( location : Map){
+              ViewController.managedContext.delete(location)
+              try! ViewController.managedContext.save()
+            }
+            
+      
+      
+            func updateRecord(location:Map,title:String,subTitle:String,latitude: Double,longitude: Double){
+                
+                
+                    location.title = title
+                    location.subTitle = subTitle
+                    location.latitude = latitude
+                    location.longitude = longitude
+              try! ViewController.managedContext.save()
+            }
+        
+    }
 
 
--- My Map/ViewController.swift
+++ My Map/ViewController.swift
@@ -11,8 +11,16 @@ import CoreData
 import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
- 
+  var selectPinView: MKAnnotation!
     static var  managedContext: NSManagedObjectContext!
+    
+    
+    var locatonSingle = Map()
+      var mTitle = ""
+         var mSubTitle = ""
+         var mLatitude = 0.0
+         var mLongitude = 0.0
+    var locationDataArray = [Map]()
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -24,10 +32,57 @@ class ViewController: UIViewController, MKMapViewDelegate {
         // Do any additional setup after loading the view.
     }
     
+    func setPins() {
+        
+        var locValue:CLLocationCoordinate2D = CLLocationCoordinate2D()
+        
+        print("latitude" + "\(locValue.latitude)")
+        print("latitude" + "\(locValue.longitude)")
+        
+        
+        var pinPoint = [MKPointAnnotation]()
+        
+       // locationDataArray = fetchRecords()
+           mapView.removeAnnotations(mapView.annotations)
+        for i in 0..<locationDataArray.count{
+            let annotation = MKPointAnnotation()
+            
+            locValue.latitude = locationDataArray[i].latitude
+            locValue.longitude = locationDataArray[i].longitude
+            
+            annotation.coordinate = locValue
+            mapView.isZoomEnabled = false
+            
+         
+            
+            annotation.title = locationDataArray[i].title
+            annotation.subtitle = locationDataArray[i].subTitle
+            
+            self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+            
+            pinPoint.append(annotation)
+            //  mapView.addAnnotation(annotation)
+        }
+        mapView.addAnnotations(pinPoint)
+        
+        let loca = CLLocationCoordinate2DMake(locValue.latitude,
+                                              locValue.longitude)
+        let coordinateRegion = MKCoordinateRegion(center: loca,
+                                                  latitudinalMeters: 4000000, longitudinalMeters: 4000000)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
     override func viewWillAppear(_ animated: Bool) {
            super.viewWillAppear(animated)
          //  setPins()
        }
-
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+       
+           
+           selectPinView = view.annotation
+    }
+    
+    
 }
 
